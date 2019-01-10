79324c0c2ee7091206a2c8ca7e665333f5db2657
unknown <prraful@gmail.com>
2019-01-10 18:03:19 +0530

    node = image write to html + master class

--- fileio.js
+++ fileio.js
@@ -5,20 +5,27 @@ http.createServer(function(rq, rs){
     rs.writeHead(200, {'Content-Type':'text/html'})
     rs.write("<h1>Working with fs module</h1>")
 
-    fs.readFile('hello.txt', function(err, data){
+    fs.readFile('banner.jpg', function(err, data){
         if(err){
             throw err
             rs.end()
         }
         rs.write("<br>")
         console.log(data)
-        rs.write(data)
+        //rs.write(data)
 
-        fs.writeFile('hello1.txt', data, function(err){
-            console.log(err)
-        })
+        //fs.mkdir(__dirname+"/images")
 
-        rs.end()
+        //fs.writeFile('images/encoded.jpg', data, function(err){})
+
+        //var b64 = data.toString('base64')
+        //var decodedImage = new Buffer(b64, 'base64')
+        
+        //fs.writeFile('images/decoded.jpg', decodedImage, function(err){})         
+
+        rs.write('<html><body><img src="data:image/jpeg;base64,')
+        rs.write(Buffer.from(data).toString('base64'))
+        rs.end('"/></body></html>')
     })
 
    
